d18ae8fda7513ee3da74ac32761a68d0bae3b151
RetroitKnife <retroitknife@protonmail.com>
2022-06-25 04:13:44 +0200

    Added Working Item

---
+++ prototypes/testArmor.lua
@@ -0,0 +1,14 @@
+--test-armor.lua
+--author: RetroitKnife
+
+local testArmor = table.deepcopy(data.raw.armor["light-armor"])
+testArmor.name = "test-armor"
+
+testArmor.icons = {
+	{
+		icon=testArmor.icon,
+		tint={r=0.5,g=0.5,b=0.5,a=0.3}
+	}
+}
+
+data:extend{testArmor}
