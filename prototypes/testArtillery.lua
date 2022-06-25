be404b1754161ae5f5aa786960343474f50c8b00
RetroitKnife <retroitknife@protonmail.com>
2022-06-25 02:53:48 +0200

    Added basic file structure Not working!

---
+++ prototypes/testArtillery.lua
@@ -0,0 +1,16 @@
+--testArtillery.lua
+--author: RetroitKnife
+
+local testArtillery = table.deepcopy(data.raw.artillery-turret["artillery-turret"])
+testArtillery.name = "test-artillery"
+
+
+testArtillery.icons = {
+	{
+		icon=testArtillery.icon,
+		tint={r=0.5,g=0.5,b=0.5,a=0.3}		
+	}
+}
+
+
+data:extend{testArtillery}
