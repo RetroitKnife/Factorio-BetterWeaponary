0a680836ba08e6984f2495c4e9bb4c1963cdd513
RetroitKnife <retroitknife@protonmail.com>
2022-06-30 15:25:13 +0200

    Works now

--- prototypes/testArtillery.lua
+++ prototypes/testArtillery.lua
@@ -4,16 +4,19 @@
 local testArtillery = table.deepcopy(data.raw["artillery-turret"]["artillery-turret"])
 testArtillery.name = "test-artillery"
 
-testArtillery.icons = {
-	{
-		icon = testArtillery.icon,
-		tint = { r = 0.5, g = 0.5, b = 0.5, a = 0.3 }		
-	}
-}
-
-testArtillery.inventory_size = 42
-testArtillery.ammo_stack_limit = 42
-testArtillery.turret_rotation_speed = 10
-testArtillery.manual_range_modifier = 42
-
 data:extend{testArtillery}
+
+data:extend(
+	{
+		{
+			type = "item",
+			name = "test-artillery",
+			icon = "__base__/graphics/icons/artillery-turret.png",
+			icon_size = 64,
+			icon_mipmaps = 4,
+			subgroup = "defensive-structure",
+			stack_size = 42,
+			place_result = "test-artillery",
+		}
+	}
+)
